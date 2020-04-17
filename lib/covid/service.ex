bb344ce766239f4c7d4b83b954def3f35ab61647
tumit <tumit@live.com>
2020-04-18 01:49:07 +0700

    change to db

--- lib/covid/service.ex
+++ lib/covid/service.ex
@@ -1,16 +1,17 @@
 defmodule Covid.Service do
     alias Covid.Virus.Bean
+    alias Covid.Repo
 
     def list_viruses do
-        [
-            %Bean{code: "001", name: "covid 19"},
-            %Bean{code: "002", name: "covid 20"},
-            %Bean{code: "003", name: "covid 21"}
-        ]
+        Bean |> Repo.all
+        # [
+        #     %Bean{code: "001", name: "covid 19"},
+        #     %Bean{code: "002", name: "covid 20"},
+        #     %Bean{code: "003", name: "covid 21"}
+        # ]
     end
 
     def get_virus_by_code(code) do
-        viruses = list_viruses
-        Enum.find(viruses, fn v -> v.code == code end)
+        Bean |> Repo.get_by(code: code)
     end
 end
